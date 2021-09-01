5a0c44af1f73846efddf7576dec527195835efb6
佐々木敏行 <sasakitoshiyuki@sasakitoshiyukinoMacBook-Air.local>
2021-09-02 03:13:58 +0900

    db_table_creation

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :message do
-    message_detail { "MyText" }
+    message_detail { 'MyText' }
     projegt { nil }
     task { nil }
   end
--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :task do
-    task_name { "MyString" }
+    task_name { 'MyString' }
     task_rep_id { 1 }
     task_report_frequency { 1 }
-    task_next_report_date { "2021-09-01" }
+    task_next_report_date { '2021-09-01' }
     task_reported_flag { false }
     projegt { nil }
   end
