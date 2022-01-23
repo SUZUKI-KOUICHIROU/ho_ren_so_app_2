7f63861b4a623fe84d676b12f7faf76b1e18b51a
佐々木敏行 <sasakitoshiyuki@sasakitoshiyukinoMacBook-Air.local>
2022-01-23 22:18:14 +0900

    page_after_login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,12 +20,12 @@ module ApplicationHelper
 
 
   # サイドバーのヘルパーメソッド
-  def sidebar_link_item(name, path)
+  def sidebar_link_item(name, path, options = {})
     class_name = 'channel'
     class_name << ' active' if current_page?(path)
 
-    content_tag :li, class:class_name do
-      link_to name, path, class: 'channel_name'
+    content_tag :li, class: class_name do
+      link_to name, path, options
     end
   end
 end
