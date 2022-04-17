2196f5fc31b7956d3b9ee279df5ce2e3c6742458
muramasa927 <snight16@icloud.com>
2022-04-17 16:41:49 +0900

    not debugger

--- app/controllers/projects/base_project_controller.rb
+++ app/controllers/projects/base_project_controller.rb
@@ -56,7 +56,6 @@ class Projects::BaseProjectController < BaseController
   
   # 仮登録のままのユーザーを編集画面へ飛ばす
   def temporarily_user?
-    debugger
     if current_user.has_editted == false
       flash[:success] = 'ユーザー名を入力してください。'
       redirect_to edit_user_registration_path(current_user)
