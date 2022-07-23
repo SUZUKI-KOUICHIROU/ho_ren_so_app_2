14d78ab1ae9d3a15858eb8a525c3715f9a3b446b
Asahi-cloud-lang <hiroto_asahi@yahoo.co.jp>
2022-07-23 17:00:37 +0900

    複数選択可の補足コメント追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,7 +16,7 @@ module ApplicationHelper
   ApplicationHelper.weeks = %w[日 月 火 水 木 金 土]
 
   ApplicationHelper.form_option = { '記述式' => 'text_field', '段落式' => 'text_area', 'ラジオボタン' => 'radio_button',
-                                    'チェックボックス' => 'check_box', 'プルダウン' => 'select', '日付' => 'date_field' }
+                                    'チェックボックス(複数選択可)' => 'check_box', 'プルダウン' => 'select', '日付' => 'date_field' }
 
 
   # サイドバーのヘルパーメソッド
