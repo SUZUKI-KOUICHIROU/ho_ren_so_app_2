3b92c2848e34a74587b5ae0fdec19ab371bb45d5
hisa883 <hisa1412@icloud.com>
2023-01-07 23:57:41 +0900

    changed name

--- app/helpers/projects/projects_helper.rb
+++ app/helpers/projects/projects_helper.rb
@@ -2,14 +2,14 @@ module Projects::ProjectsHelper
   def project_leader?
     return current_user.id == @project.leader_id
   end
-  def report_users(project, report_deadline)
-    report_users_id = []
+  def reported_users(project, report_deadline)
+    reported_users_id = []
     i = 0
-    report_users = project.reports.where(report_day: report_deadline.day).where(remanded: false).select(:user_id).distinct
-    report_users.each do |user|
-      report_users_id[i] = user.user_id
+    reported_users = project.reports.where(report_day: report_deadline.day).where(remanded: false).select(:user_id).distinct
+    reported_users.each do |user|
+      reported_users_id[i] = user.user_id
       i += 1
     end
-    return project.users.all.where(id: report_users_id)
+    return project.users.all.where(id: reported_users_id)
   end
 end
