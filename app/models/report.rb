5538b08e6a67ef65791c01f5a83b904a790be661
shinya-okuno <shinya.works.w@gmail.com>
2023-07-13 10:38:50 +0900

    全プロジェクト報告集計モデル＆コントローラー実装

--- app/models/report.rb
+++ app/models/report.rb
@@ -12,4 +12,12 @@ class Report < ApplicationRecord
   validates :title, presence: true, length: { maximum: 30 }
   validates :report_day, presence: true
   validates :sender_name, presence: true
+
+  def self.befor_deadline_reports_size(project_reports)
+    if project_reports.present?
+      return project_reports.map { |report| report.report_day == report.created_at.to_date ? report.user_id : nil }.compact.uniq.size
+    else
+      return 0
+    end
+  end
 end
