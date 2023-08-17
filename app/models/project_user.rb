216231e5669ffb66ff7872914b2694d99fe7152f
shinya-okuno <shinya.works.w@gmail.com>
2023-08-17 22:50:39 +0900

    プロジェクトメンバーの報告集計からの除外機能実装

--- app/models/project_user.rb
+++ app/models/project_user.rb
@@ -1,4 +1,13 @@
 class ProjectUser < ApplicationRecord
   belongs_to :user
   belongs_to :project
+
+  # プロジェクトメンバーそれぞれに報告集計メンバーの除外ステータスを追加する
+  def self.member_expulsion_join(project, members)
+    project_member_expulsions = project.project_users.all
+    members.each do |member|
+      member.member_expulsion = project_member_expulsions.find(member.id).member_expulsion
+    end
+    return members
+  end
 end
