32ca4d3163b44f39d05c5a55be33f75b10158358
katsue-watanabe <k-losmama@docomo.ne.jp>
2023-09-18 08:56:22 +0900

    commit
    change.monthiy-weekly

--- app/controllers/projects/reports_controller.rb
+++ app/controllers/projects/reports_controller.rb
@@ -5,10 +5,23 @@ class Projects::ReportsController < Projects::BaseProjectController
     set_project_and_members
     @first_question = @project.questions.first
     @report_label_name = @first_question.send(@first_question.form_table_type).label_name
-    @reports = @project.reports.where.not(sender_id: @user.id).order(updated_at: 'DESC').page(params[:page]).per(10)
-    @you_reports = @project.reports.where(sender_id: @user.id).order(updated_at: 'DESC').page(params[:page]).per(10)
-    @monthly_reports = Report.monthly_reports_for(@project, @user, params[:page])
-    @weekly_reports = Report.weekly_reports_for(@project, @user, params[:page])
+    @reports = @project.reports.where.not(sender_id: @user.id).order(created_at: 'DESC').page(params[:page]).per(10)
+    @you_reports = @project.reports.where(sender_id: @user.id).order(created_at: 'DESC').page(params[:page]).per(10)
+    @monthly_reports = Report.monthly_reports_for(@project, @user)
+    @weekly_reports = Report.weekly_reports_for(@project, @user)
+    if params[:report_type] == 'monthly'
+      @reports = Report.monthly_reports_for(@project, @user)
+      @reports = @monthly_reports.where.not(sender_id: @user.id).order(created_at: 'DESC').page(params[:page]).per(10)
+      @you_reports = @monthly_reports.where(sender_id: @user.id).order(created_at: 'DESC').page(params[:page]).per(10)
+      { reports: @reports, you_reports: @you_reports }
+    elsif params[:report_type] == 'weekly'
+      @reports = Report.weekly_reports_for(@project, @user)
+      @reports = @weekly_reports.where.not(sender_id: @user.id).order(created_at: 'DESC').page(params[:page]).per(10)
+      @you_reports = @weekly_reports.where(sender_id: @user.id).order(created_at: 'DESC').page(params[:page]).per(10)
+      { reports: @reports, you_reports: @you_reports }
+    else
+      render :index     
+    end
     if params[:search].present? and params[:search] != ""
       @results = Report.search(report_search_params)
       if @results.present?
