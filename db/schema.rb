db76db17f19d6510668f4d9efecf5cb91ec527bd
suzuki <k.s@suzuki-kouichirounoMacBook-Air.local>
2023-12-08 19:56:21 +0900

    Resolve merge conflict in db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_07_121602) do
+ActiveRecord::Schema.define(version: 2023_12_04_002627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
