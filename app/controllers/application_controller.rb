c4d378a6a5bf5e21e5fafafdc9cc25c6dc8977da
suzuki <k.s@suzuki-kouichirounoMacBook-Air.local>
2023-12-08 20:34:57 +0900

    RuboCop修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   end
 
   def storable_location?
-    request.get? && !devise_controller? && !request.xhr? && !root_path.match(/\/users/)
+    request.get? && !devise_controller? && !request.xhr? && root_path.exclude?('/users')
   end
 
   def store_location_for(resource_or_scope, location)
@@ -16,10 +16,10 @@ class ApplicationController < ActionController::Base
   def user_has_access?(user, path)
     counseling_id = extract_counseling_id_from_path(path)
     return false unless counseling_id
-  
+
     counseling = Counseling.find_by(id: counseling_id)
     return false unless counseling
-  
+
     counseling.project.users.include?(user)
   end
 
