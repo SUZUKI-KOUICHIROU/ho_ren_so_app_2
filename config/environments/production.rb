e92a29f90aa1c484468cd70855dbba135b6a397a
山田幸助 <offmailbox1225@gmail.com>
2024-01-12 02:35:18 +0900

    Action Mailerの本番環境用の設定 ⇨ hostのURLを指定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   
-  host = 'horenso-app.fly.dev/'
+  host = 'horenso-app.fly.dev'
   # config.action_mailer.default_url_options = { host: host }
   config.action_mailer.default_url_options = { protocol: 'https',host: host}
   ActionMailer::Base.smtp_settings = {
