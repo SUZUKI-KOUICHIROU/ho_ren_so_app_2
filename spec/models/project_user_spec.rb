fbb553a01fc2c834a8f1c38f6b5bc930624022ab
Brandon Heat <stardustgrave100msn@ybb.ne.jp>
2024-03-21 10:08:03 +0900

    プロジェクトユーザーモデル用のspecファイル＆factoryファイル生成

--- spec/models/project_user_spec.rb
+++ spec/models/project_user_spec.rb
@@ -1,15 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe ProjectUser, type: :model do
-  describe '計算' do
-    context '10 + 10の場合' do
-      it '20になること' do
-       expect(10 + 10).to eq 20
-      end
-
-      it '30にはならない' do
-       expect(10 + 10).not_to eq 30
-      end
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
