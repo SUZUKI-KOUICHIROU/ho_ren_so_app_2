4290fb904c8488ff125579115637b174c524458a
Brandon Heat <stardustgrave100msn@ybb.ne.jp>
2024-03-27 11:09:13 +0900

    プロジェクトユーザーのmember_expulsion_joinメソッドに対するモデルスペック実装

--- spec/factories/project_users.rb
+++ spec/factories/project_users.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :project_user do
-    
+    association :user, factory: :unique_user
+    association :project
+
+    member_expulsion { false } # member_expulsion のデフォルト値を指定
+  end
+end
+
+FactoryBot.define do
+  factory :unique_user, class: 'User' do
+    name { Faker::Name.name } # ランダムな名前を生成
+    email { Faker::Internet.unique.email } # ランダムなメールアドレスを生成
+    password { 'password' }
+    password_confirmation { 'password' }
   end
 end
--- spec/models/project_user_spec.rb
+++ spec/models/project_user_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe ProjectUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'member_expulsion_joinメソッドのテスト' do
+    context '各メンバーに正しく除外ステータスを割り当てる' do
+      it '除外したメンバーには除外ステータスが割り当てられる' do
+        # テストプロジェクトの作成
+        project = FactoryBot.create(:project)
+    
+        # テストユーザーの作成
+        member1 = FactoryBot.create(:unique_user)
+        member2 = FactoryBot.create(:unique_user)
+        
+        # テストプロジェクトへのメンバー関連付け
+        project_user1 = FactoryBot.create(:project_user, project: project, user: member1)
+        project_user2 = FactoryBot.create(:project_user, project: project, user: member2)
+
+        # 除外ステータスの設定
+        project_user1.update(member_expulsion: true)
+        project_user2.update(member_expulsion: false)
+        
+        # メンバー配列の作成
+        members = [member1, member2]
+        
+        # member_expulsion_joinメソッドの呼び出し
+        ProjectUser.member_expulsion_join(project, members)
+  
+        # 各メンバーの除外ステータスを再度取得して検証
+        members.each(&:reload)
+      
+        # メンバーのmember_expulsionメソッドが正しい設定かを検証
+        expect(members[0].member_expulsion).to eq(true)
+        expect(members[1].member_expulsion).to eq(false)
+    
+      end
+    end
+  end
 end
