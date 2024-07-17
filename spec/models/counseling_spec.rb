f50a48f64488a945c6f48aec0d9edcc01422fc48
shoutaro <850123.a@gmail.com>
2024-07-17 23:33:37 +0900

    conseling_spec.rb 相談検索　空で検索時全相談表示を追加

--- spec/models/counseling_spec.rb
+++ spec/models/counseling_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.describe Counseling, type: :model do
   subject(:message) { FactoryBot.build(:message) }
 
-  describe 'counselingの登録' do
-    context '相談の送信' do
+  describe 'counselingの登録' do # describeはテスト主題
+    context '相談の送信' do # 条件
       before(:each) do
         # テスト用のプロジェクトを作成し、インスタンス変数に格納
-        @project = create(:project)
+        @project = create(:project) # これは本ファイルの他のテストでもプロジェクト作成を適用させるためのもの
       end
-      it '送信相手、件名、相談内容があれば送信できる' do
-        expect(build(:counseling, title: 'test', counseling_detail: 'test', send_to_all: true)).to be_valid
+      it '送信相手、件名、相談内容があれば送信できる' do # it テスト内容の題名
+        expect(build(:counseling, title: 'test', counseling_detail: 'test', send_to_all: true)).to be_valid # expect:期待値 to_be_valid:期待した結果になる(true)
       end
       it 'titleがなければ送信できない' do
-        expect(build(:counseling, title: '')).to be_invalid
+        expect(build(:counseling, title: '')).to be_invalid # to_be_invalid falseになる
       end
       it '件名が31文字以上は送信できない' do
         expect(build(:counseling, title: 'a' * 31)).to be_invalid
@@ -30,6 +30,13 @@ RSpec.describe Counseling, type: :model do
       it '送信相手が空の場合は送信できない' do
         expect(build(:counseling, send_to_all: 'false' && :counseling, send_to: '')).to be_invalid
       end
+    end
+  end
+
+  describe '相談検索' do
+    let(:search_params) { { keywords: 'example' } }
+    it '検索が空で実行された場合、全相談を表示する' do
+      expect(Counseling.search({})).to eq(Counseling.all)
     end
   end
 end
