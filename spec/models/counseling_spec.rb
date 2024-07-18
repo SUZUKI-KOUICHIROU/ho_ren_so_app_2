9918cbdb3b6018be84cea4f0d54003901c7ddcee
shoutaro <850123.a@gmail.com>
2024-07-18 10:36:52 +0900

    model/counseling_spec.rb コメント精査

--- spec/models/counseling_spec.rb
+++ spec/models/counseling_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.describe Counseling, type: :model do
   subject(:message) { FactoryBot.build(:message) }
 
-  describe 'counselingの登録' do # describeはテスト主題
-    context '相談の送信' do # 条件
+  describe 'counselingの登録' do
+    context '相談の送信' do
       before(:each) do
         # テスト用のプロジェクトを作成し、インスタンス変数に格納
-        @project = create(:project) # これは本ファイルの他のテストでもプロジェクト作成を適用させるためのもの
+        @project = create(:project)
       end
-      it '送信相手、件名、相談内容があれば送信できる' do # it テスト内容の題名
+      it '送信相手、件名、相談内容があれば送信できる' do
         expect(build(:counseling, title: 'test', counseling_detail: 'test', send_to_all: true)).to be_valid # expect:期待値 to_be_valid:期待した結果になる(true)
       end
       it 'titleがなければ送信できない' do
-        expect(build(:counseling, title: '')).to be_invalid # to_be_invalid:falseになる
+        expect(build(:counseling, title: '')).to be_invalid
       end
       it '件名が31文字以上は送信できない' do
         expect(build(:counseling, title: 'a' * 31)).to be_invalid
