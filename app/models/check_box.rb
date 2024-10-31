81784aec02c298be9bf284b8797f5d2cf19c28f8
shoutaro <850123.a@gmail.com>
2024-10-31 22:24:09 +0900

    ﾁｪｯｸﾎﾞｯｸｽ 項目名に関するﾊﾞﾘﾃﾞｰｼｮﾝでの文字数制限追加

--- app/models/check_box.rb
+++ app/models/check_box.rb
@@ -3,6 +3,6 @@ class CheckBox < ApplicationRecord
   has_many :check_box_option_strings, dependent: :destroy
   accepts_nested_attributes_for :check_box_option_strings, allow_destroy: true
 
-  validates :label_name, presence: true
+  validates :label_name, presence: true, length: { maximum: 30 }
   validates :field_type, presence: true
 end
