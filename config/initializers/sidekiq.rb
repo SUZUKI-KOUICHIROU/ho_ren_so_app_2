72c9c5b99aa3a6b93a42ae1a81075b46490505c1
山田幸助 <offmailbox1225@gmail.com>
2024-10-31 10:40:16 +0900

    Sidekiqの本番環境対応 ⇨ SSL接続でのサーバー証明書のチェックを無効化

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,11 +3,17 @@ redis_config = { url: 'redis://redis:6379/0' }
 
 # Sidekiqサーバーの設定
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'] || redis_config[:url] }
+  config.redis = { 
+    url: ENV['REDIS_URL'] || redis_config[:url],
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+   }
   config.logger = Logger.new(File.join(Rails.root, 'log', 'sidekiq.log'))
 end
 
 # Sidekiqクライアントの設定
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'] || redis_config[:url] }
+  config.redis = { 
+    url: ENV['REDIS_URL'] || redis_config[:url],
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+   }
 end
